8f7ba70316cb24c080c25e5b96868d7978d0d46f
sayurabh <sayurabh@host.local>
2015-01-29 08:17:15 -0800

    added a new test file

---
+++ Git Test/CCTest.h
@@ -0,0 +1,14 @@
+
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by sayurabh on 1/29/15.
+//  Copyright (c) 2015 code colation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest:NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,16 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by sayurabh on 1/29/15.
+//  Copyright (c) 2015 code colation. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+
+
+@end
+
